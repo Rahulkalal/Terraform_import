39176ab4009432fb693243478874b57c9bef1621
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-22 18:46:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-resource "aws_s3_bucket" "test_bucket_hcl_import_hands_on" {
-  # (resource arguments)
+resource "aws_s3_bucket" "unique-terraform-state-bucket-hcl-3210" {
+  # Declare the S3 bucket resource here.
+  # You don't need to define everything (just the name is enough to import).
 }
 
-#when trying to import s3 bucket you need to import aws_s3_bucket_acl resource block also or eles you will not be importing all the attributes of s3 bucket from AWS
 resource "aws_s3_bucket_acl" "import_example" {
-  bucket = aws_s3_bucket.unique-terraform-state-bucket-hcl-3210  # Reference to the imported S3 bucket
-
-  # Optionally set the ACL (e.g., private, public-read)
+  bucket = aws_s3_bucket.unique-terraform-state-bucket-hcl-3210.id  # Reference the imported S3 bucket
   acl    = "private"
 }
