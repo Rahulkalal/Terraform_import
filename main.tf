f90d49b632a2e4d012c773ad314b24ee0a268c6b
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-22 18:36:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "aws_s3_bucket" "test_bucket_hcl_import_hands_on" {
 }
 
 #when trying to import s3 bucket you need to import aws_s3_bucket_acl resource block also or eles you will not be importing all the attributes of s3 bucket from AWS
-resource "aws_s3_bucket_acl" "import_example"
+resource "aws_s3_bucket_acl" "import_example" {
+}
