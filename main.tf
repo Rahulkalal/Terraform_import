a7b0d346ea612c780ee024cac1365eae3b2bcb7d
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-22 18:29:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "hands_on_hcl" {
-  # Placeholder for the EC2 instance.
-  # Add required fields like `ami` and `instance_type` after the import
-  ami           = "ami-0e2c8caa4b6378d8c"  # Replace with actual AMI ID
-  instance_type = "t2.micro"              # Replace with actual instance type
+resource "aws_s3_bucket" "test_bucket_hcl_import_hands_on" {
+  # (resource arguments)
 }
